4bc83423be41ee493b291fad7b3da91421840405
aobi <aobi@aobis-MacBook-Air.local>
2014-06-29 00:34:48 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -17,8 +17,12 @@
 }
 
 // put new methods here
-(void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event{
+//(void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event{
     //this will get called every time the player touches the screen
+//}
+
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+    // this will get called every time the player touches the screen
 }
 
 @end
